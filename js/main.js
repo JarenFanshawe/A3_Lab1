6cfa0aeda27ebf87c7f6b1574bfafd79769b87d4
jarenmcdonald <estyn72@gmail.com>
2017-12-11 16:26:08 -0500

    added more stuff

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
 (function() {
 
-	var theImages = document.querySelectorAll('.data-ref');
+	var carButtons = document.querySelectorAll('.data-ref');
 
-	function getChangeElements() {
+	function getCarData() {
 		const url = './includes/functions.php?carModel=' + this.id;
 
 		fetch(url) //do an AJAX call with fetch
 		.then((resp) => resp.json()) //convert to JSON
-		.then({ modelName, priceing, modelDetails } => {
-			processResult(data);
+		.then(({ modelName, pricing, modelDetails, model }) => {
+			let carModel = document.querySelector('.modelName').textContent = modelName;
+			let price = document.querySelector('.priceInfo').innerHTML = pricing;
+			let desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+			carButtons.forEach(function(image, index){
+				image.classList.add("nonActive");
+			});
+
+			document.querySelector(`#${model}`).classList.remove("nonActive");
 		}) // call the process function
+
 		.catch(function(error) {
 			console.log(error);
 		});
+}
 
-	function processResult(data) {
-		const { modelName, pricing, modelDetails} = data;
-
-		let model = document.querySelector('.modelName').textContent = modelName;
-		let price = document.querySelector('.priceInfo').innerHTML = pricing;
-		let desc = document.querySelector('.modelDetails').textContent = modelDetails;
-
-		theImages.forEach(function(image, index){
-			image.classList.add("nonActive");
-		});
-
-		document.querySelector(`#${data.model}`).classList.remove("nonActive");
-	}
-
-	theImages.forEach(function(image, index){
-		image.addEventListener('click', getChangeElements, false);
+	carButtons.forEach(function(image, index){
+		image.addEventListener('click', getCarData, false);
 	});
 
 	//document.querySelector('#spring').click();
-	getChangeElements.call(document.querySelector("#F55"));
+	getCarData.call(document.querySelector("#F55"));
 
 
 })();
